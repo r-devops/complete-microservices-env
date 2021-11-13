8e183e7a676ece6529f904f9b791172c170bf8f8
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2021-11-13 20:18:47 +0530

    ss

---
+++ financial/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+type mvn
+if [ $? -ne 0 ]; then
+  curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/maven/install-jdk11.sh | bash
+fi
+mvn -Dmaven.test.skip clean package
+export SPRING_PROFILES_ACTIVE=prod
+java -jar target/financial-0.0.1-SNAPSHOT.jar
