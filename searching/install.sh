28af412d7d70a67bc2912c5c12898f0a86ee7192
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2021-11-13 19:37:18 +0530

    ss

--- searching/install.sh
+++ searching/install.sh
@@ -4,6 +4,6 @@ type mvn
 if [ $? -ne 0 ]; then
   curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/maven/install-jdk11.sh | bash
 fi
-mvn clean package
+mvn -Dmaven.test.skip clean package
 export SPRING_PROFILES_ACTIVE=prod
 java -jar target/searching-service-0.0.1-SNAPSHOT.jar
