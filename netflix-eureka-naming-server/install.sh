2163ed7fab2b52fbb76d4a6cb803f1c69fc28f51
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2021-11-13 19:27:45 +0530

    ss

--- netflix-eureka-naming-server/install.sh
+++ netflix-eureka-naming-server/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/maven/install-jdk11.sh | bash
 mvn clean package
+java -jar target/netflix-eureka-naming-server-0.0.1-SNAPSHOT.jar
